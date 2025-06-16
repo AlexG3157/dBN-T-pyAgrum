ce1169d87a4bba4da04d9346e0f0f7e686f141c3
Alex Gomez <alex3157@yahoo.com>
2025-06-16 08:51:47 +0200

    Experiments

--- experiments/grid_search.py
+++ experiments/grid_search.py
@@ -45,21 +45,23 @@ def log_result(config_dict, metrics, path="grid_results.csv"):
         writer.writerow(result)
 
 
-file_path = "data/learner_ntraj_length.csv"
+file_path = "data/learned_k_hist2.csv"
 
 #Search space
-k_range = [2,5,7,10]
-n_traj_range = [100,500,1500,2500]
-traj_length_range = [11,20,35,50]
+k_range = [3]
+n_traj_range = [20]
+traj_length_range = [20]
+max_k = 12
 
 n_vars_range = [4]
-n_mods_range =[3]
-density_range = [0.5]
+n_mods_range =[4]
+density_range = [0.1]
 
-repetitions = 20
+repetitions = 2000
 
-do_learner = True
-do_KLearner = False
+do_skeleton_learner = False
+do_proba_learner = False
+do_KLearner = True
 
 for mods in n_mods_range:
     for density in density_range:
@@ -75,7 +77,8 @@ for mods in n_mods_range:
                                   'k':k, 
                                   'n_traj':n_traj, 
                                   'traj_length':traj_length,
-                                  'do_learner' : do_learner,
+                                  'do_proba_learner' : do_proba_learner,
+                                  'do_skeleton_learner' : do_skeleton_learner,
                                   'do_KLearner' : do_KLearner
                                   }
                         
@@ -88,13 +91,13 @@ for mods in n_mods_range:
                         for i in range(repetitions):
                             
                             n_arcs = int(density*(k/2)*((k-1)*n_vars**2 + n_vars*(n_vars-1)))
-
+                            
                             true_ktbn = KTBN.random(k,n_vars,mods,n_arcs, delimiter='_')
                             trajs = true_ktbn.sample(n_traj, traj_length)
 
                             data = dict()
 
-                            if do_learner:
+                            if do_proba_learner or do_skeleton_learner:
 
                                 learner = Learner(trajs, Discretizer(),delimiter='_', k=k)
 
@@ -107,13 +110,14 @@ for mods in n_mods_range:
                                 true_bn = true_ktbn.to_bn()
                                 learned_bn = learned_ktbn.to_bn()
 
-                                g = gum.GibbsBNdistance(true_bn, learned_bn)
-                                g.setVerbosity(True)
-                                g.setMaxTime(120)
-                                g.setBurnIn(5000)
-                                g.setEpsilon(1e-7)
-                                g.setPeriodSize(500)
+                            if do_proba_learner:
+                                
+                                g = gum.ExactBNdistance(true_bn, learned_bn)
+                            
                                 data.update(g.compute())
+                                data.update({'learning_time' : learning_time})
+
+                            if do_skeleton_learner:
 
                                 cm = gcm.GraphicalBNComparator(true_bn, learned_bn)
 
